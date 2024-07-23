6571aad8cef2e964d9f712e024d9003f67d3daaa
HarshilMalhotra <malhotraharshil2@gmail.com>
2024-07-24 01:14:35 +0530

    Reorganising structure 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import './App.css'
+import './App.css';
 import { Link } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const App = () => {
     return (
-        <div className='bg-black text-white'>
-            <h1>Welcome!</h1>
-            <Link to="/register">Register</Link>
-            <br />
-            or
-            <br />
-            <Link to="/login">Login</Link>
+        <div>
+            <div className='bg-black text-white'>
+                <h1>Welcome!</h1>
+                <Link to="/register">Register</Link>
+                <br />
+                or
+                <br />
+                <Link to="/login">Login</Link>
 
+                <marquee color="red" scrollamount="12" className='text-red-600 text-xl font-bold'>
+                    This is a component based website. all the pages are present in routes folder. and page components are present in components folder. Kindly follow the same file structue for making any additions to the page.
+                </marquee>
+            </div>
 
-            <marquee color="red" scrollamount="12" className='text-red-600 text-xl font-bold'>
-                This is a component based website. all the pages are present in routes folder. and page components are present in components folder. Kindly follow the same file structue for making any additions to the page.
-            </marquee>
-          
+            <div>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/vibe">Vibe Check</Link>
+                <Link to="/overview">Overview</Link>
+                <Link to="/problem">Problem</Link>
+            </div>
         </div>
-
-
-
     );
 };
 
 export default App;
-
-
-
-
-
